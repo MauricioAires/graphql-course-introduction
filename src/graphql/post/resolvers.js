7a375c1d3e9819848fb2272463b1f3a943b413cc
MauricioAires <mauricioaires015@gmail.com>
2023-03-21 15:36:27 -0300

    feat: resolvedor trivial

--- src/graphql/post/resolvers.js
+++ src/graphql/post/resolvers.js
@@ -14,9 +14,28 @@ export const posts = async (obj, args, { getPosts }, info) => {
   return data;
 };
 
+/**
+ * Em GraphQL, um resolvedor trivial é uma função que é capaz de retornar
+ * diretamente o valor desejado sem precisar realizar nenhum processamento
+ * adicional. Por exemplo, se uma consulta GraphQL solicita o nome de um usuário
+ * e esse nome já está disponível em uma variável, um resolvedor trivial pode
+ * simplesmente retornar o valor da variável sem precisar fazer qualquer busca ou
+ * manipulação de dados. Em resumo, um resolvedor trivial é uma função que resolve uma
+ * consulta GraphQL de forma direta e rápida, sem a necessidade de realizar cálculos
+ * complexos ou acessar bancos de dados externos.
+ *
+ * Aplicabilidade: o primeiro parâmetro content os dados retornados para cada
+ * objeto, dessa forma pode ser utilizado para formatar valores ou concatenar um
+ * nome e sobre nome
+ */
 export const postResolvers = {
   Query: {
     post,
     posts,
+  },
+  Post: {
+    unixTimestamp: ({ createdAt }) => {
+      return new Date(createdAt).getTime();
+    },
   },
 };
--- src/graphql/post/typedefs.js
+++ src/graphql/post/typedefs.js
@@ -13,5 +13,6 @@ export const postTypeDefs = gql`
     #userId:
     indexRef: Int!
     createdAt: String!
+    unixTimestamp: String!
   }
 `;
