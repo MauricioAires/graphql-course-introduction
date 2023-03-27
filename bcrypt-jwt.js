270d3dff6c09aa80306f8858d949adb4ea2178c8
MauricioAires <mauricioaires015@gmail.com>
2023-03-27 13:49:11 -0300

    chore: bcrypt and json web token examples

---
+++ bcrypt-jwt.js
@@ -0,0 +1,36 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+/**
+ * NOTE: como compilar esse arquivo =>  yarn nodemon --exec babel-node bcrypt-jwt.js
+ */
+
+(async () => {
+  const password = '12345';
+
+  const passwordHash = await bcrypt.hash(password, 12);
+
+  const userLoginPassword = '123456';
+
+  const passwordIsValid = await bcrypt.compare(userLoginPassword, passwordHash);
+
+  console.log(passwordHash, passwordIsValid);
+})();
+
+(async () => {
+  const token = jwt.sign(
+    {
+      userId: '123',
+    },
+    'O_MEU_SECRET',
+    {
+      expiresIn: '7d',
+    },
+  );
+
+  console.log(token);
+
+  const tokenData = jwt.verify(token, 'O_MEU_SECRET');
+
+  console.log(tokenData);
+})();
