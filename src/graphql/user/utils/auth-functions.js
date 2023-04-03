d021e349ebd11c2afd46caaaa80a10d37d9030c6
MauricioAires <mauricioaires015@gmail.com>
2023-04-03 16:51:45 -0300

    feat(user) auth function check is owner and logged in

---
+++ src/graphql/user/utils/auth-functions.js
@@ -0,0 +1,16 @@
+import { AuthenticationError } from "apollo-server";
+
+export const checkIsLoggedIn = (userId, loggedUserId) => {
+  if (!loggedUserId) {
+    throw new AuthenticationError('You must be logged in!');
+  }
+}
+
+export const checkOwner = (userId, loggedUserId) => {
+
+  checkIsLoggedIn()
+
+  if (loggedUserId !== userId) {
+    throw new AuthenticationError('You cannot delete this user!');
+  }
+}
